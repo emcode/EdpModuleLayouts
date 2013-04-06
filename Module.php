e0535de01d0e3c3e4990ecbacbdd4d9e15f32311
emcode <michal@simplercode.com>
2013-04-06 15:28:33 +0300

    Added support for more complex module names like "Example\ModuleName"

--- Module.php
+++ Module.php
@@ -8,7 +8,7 @@ class Module
         $e->getApplication()->getEventManager()->getSharedManager()->attach('Zend\Mvc\Controller\AbstractActionController', 'dispatch', function($e) {
             $controller      = $e->getTarget();
             $controllerClass = get_class($controller);
-            $moduleNamespace = substr($controllerClass, 0, strpos($controllerClass, '\\'));
+            $moduleNamespace = substr($controllerClass, 0, strpos($controllerClass, '\\Controller'));
             $config          = $e->getApplication()->getServiceManager()->get('config');
             if (isset($config['module_layouts'][$moduleNamespace])) {
                 $controller->layout($config['module_layouts'][$moduleNamespace]);
