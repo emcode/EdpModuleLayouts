e9d3cf19910ba2a3cc1091eb0519e45778608224
emcode <michal@simplercode.com>
2013-08-28 01:47:04 +0200

    Changed the way of matching namespaces of Module and Controller
    - we do not rely on \\Controller subnamespace anymore
    - we have more freedom when specyfying module => layout mapping
      as only beginning of namespace have to match
    - remember to set most specific aliases as first, under
      module_layouts config key

--- Module.php
+++ Module.php
@@ -6,13 +6,27 @@ class Module
     public function onBootstrap($e)
     {
         $e->getApplication()->getEventManager()->getSharedManager()->attach('Zend\Mvc\Controller\AbstractController', 'dispatch', function($e) {
-            $controller      = $e->getTarget();
+
+            $controller = $e->getTarget();
             $controllerClass = get_class($controller);
-            $moduleNamespace = substr($controllerClass, 0, strpos($controllerClass, '\\Controller'));
-            $config          = $e->getApplication()->getServiceManager()->get('config');
-            if (isset($config['module_layouts'][$moduleNamespace])) {
-                $controller->layout($config['module_layouts'][$moduleNamespace]);
+            $config = $e->getApplication()->getServiceManager()->get('config');
+
+            if (!isset($config['module_layouts']))
+            {
+                return;
             }
+
+            $moduleLayouts = $config['module_layouts'];
+
+            foreach($moduleLayouts as $moduleNamespace => $layoutName)
+            {
+               if (strpos($controllerClass, $moduleNamespace) === 0)
+               {
+                   $controller->layout($layoutName);
+                   break;
+               }
+            }
+
         }, 100);
     }
 }
